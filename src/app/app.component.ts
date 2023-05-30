166d6f124caed3febde36fbd019b5b16ddc9b8c6
Prosenjeet Paul <prosenjeet123@gmail.com>
2023-05-30 15:31:30 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sgpmail-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sgpmail';
+}
